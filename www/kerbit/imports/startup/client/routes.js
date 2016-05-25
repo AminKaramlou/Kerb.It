1402d980218adfaf68bfaa1115bc59eec986f6b8
DANIEL GRUMBERG <dany.grumberg@gmail.com>
2016-05-25 21:03:02 +0100

    Refactored routing but don't know why it works

--- www/kerbit/imports/startup/client/routes.js
+++ www/kerbit/imports/startup/client/routes.js
@@ -1,11 +1,8 @@
-// Import to load these templates
-import '../../ui/components/header.js';
-import '../../ui/pages/footer.js';
-import '../../ui/pages/register.js';
-
+import '../../ui/pages/';
+import '../../ui/layouts/main.js';
 
 Router.route('/', {
-    template: 'home'
+  template: 'home'
 });
 
 Router.route('/register');
