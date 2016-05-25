8d8aedccb3c82e36dc9eac838295e37f804492b2
Tom Szyszko <ts3413@ic.ac.uk>
2016-05-25 19:46:10 +0100

    Created JS files to allow pages to render

--- www/kerbit/imports/startup/client/routes.js
+++ www/kerbit/imports/startup/client/routes.js
@@ -1,3 +1,9 @@
+// Import to load these templates
+import '../../ui/components/header.js';
+import '../../ui/pages/footer.js';
+import '../../ui/pages/register.js';
+
+
 Router.route('/', {
     template: 'home'
 });
