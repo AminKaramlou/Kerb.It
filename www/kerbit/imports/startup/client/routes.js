9d9c72f154307b1eca4558543e421cdf71ee6902
Saurav  Mitra <saurav.mitra14@imperial.ac.uk>
2016-05-26 17:11:52 +0100

    Remove redundant home route

--- www/kerbit/imports/startup/client/routes.js
+++ www/kerbit/imports/startup/client/routes.js
@@ -24,10 +24,6 @@ AccountsTemplates.configureRoute('signIn');
 AccountsTemplates.configureRoute('signUp');
 AccountsTemplates.configureRoute('verifyEmail');
 
-
-Router.route('/', {
-    template: 'home'
-});
 // // Content Protection
 // Router.plugin('ensureSignedIn', {
 //     except: ['home', 'atSignIn', 'atSignUp', 'atForgotPassword','main']
