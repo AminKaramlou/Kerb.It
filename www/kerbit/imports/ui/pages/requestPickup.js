9085264e1bea3549413410b1190bed5e47d7ac8d
Saurav  Mitra <saurav.mitra14@imperial.ac.uk>
2016-05-27 08:35:21 +0100

    Add relevant fields to form in requestPickup, and event handler to call makeRequest Method

--- www/kerbit/imports/ui/pages/requestPickup.js
+++ www/kerbit/imports/ui/pages/requestPickup.js
@@ -1,1 +1,17 @@
-import "./requestPickup.html";
+import "./requestPickup.html";
+
+Template.requestPickup.events({
+  'submit form'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+
+    const title = target.title.value;
+    const description = target.description.value;
+    const bidWindow = Number(target.bidWindow.value);
+    const sizeRequired = Number(target.sizeRequired.value);
+    const postcode = target.postcode.value;
+
+    Meteor.call('makeRequest', this._id, title, description, bidWindow, sizeRequired, postcode);
+  }
+});
