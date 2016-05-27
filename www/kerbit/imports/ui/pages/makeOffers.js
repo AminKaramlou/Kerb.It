3febe4df3e293e1f288d42252d6df17b59bc2603
Saurav  Mitra <saurav.mitra14@imperial.ac.uk>
2016-05-27 13:48:27 +0100

    Add requests() helper to makeOffers

--- www/kerbit/imports/ui/pages/makeOffers.js
+++ www/kerbit/imports/ui/pages/makeOffers.js
@@ -1,11 +1,18 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
+import { Requests } from '../../api/collections.js';
 import '../../api/methods.js';
 import "./makeOffers.html";
 
 Template.makeOffers.onCreated(function driverHomeOnCreated() {
   Meteor.subscribe('requests');
+});
+
+Template.makeOffers.helpers({
+  requests() {
+    return Requests.find({});
+  }
 });
 
 Template.makeOffers.events({
