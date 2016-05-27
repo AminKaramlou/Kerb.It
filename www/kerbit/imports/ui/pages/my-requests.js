ac48d897aeb8ef8eaf40ea80f7a3111352e886a2
Saurav  Mitra <saurav.mitra14@imperial.ac.uk>
2016-05-27 07:11:54 +0100

    Combine templates in my-requests

--- www/kerbit/imports/ui/pages/my-requests.js
+++ www/kerbit/imports/ui/pages/my-requests.js
@@ -5,18 +5,13 @@ import './my-requests.html';
 
 Template.myRequests.onCreated(function myRequestsCreated() {
   Meteor.subscribe('requests'); 
-});
-
-Template.req.onCreated(function reqCreated() {
   Meteor.subscribe('offers');
 });
 
 Template.myRequests.helpers({
   requests() {
     return Requests.find({});
-  }
-});
-Template.req.helpers({
+  },
   offers(requestId) {
     return Offers.find({
       requestId
