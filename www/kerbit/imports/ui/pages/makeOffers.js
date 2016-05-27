10d26fa864b5b835be0fcc8996bad044330471cd
Saurav  Mitra <saurav.mitra14@imperial.ac.uk>
2016-05-27 07:45:40 +0100

    Call makeOffer Method on form submission with int-parsed price in makeOffers

--- www/kerbit/imports/ui/pages/makeOffers.js
+++ www/kerbit/imports/ui/pages/makeOffers.js
@@ -1,1 +1,19 @@
-import { Meteor } from 'meteor/meteor';import { Template } from 'meteor/templating';import '../../api/methods.js';import "./makeOffers.html";Template.makeOffers.onCreated(function driverHomeOnCreated() {  Meteor.subscribe('requests');});Template.makeOffers.events({  'click .submit-offer'(event) {    event.preventDefault();        const target = event.target;    console.log(target);    Meteor.call('makeOffer', "S6MTpPs8HENgS9RXk", Meteor.userId(), 3000);  }});
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+import '../../api/methods.js';
+import "./makeOffers.html";
+
+Template.makeOffers.onCreated(function driverHomeOnCreated() {
+  Meteor.subscribe('requests');
+});
+
+Template.makeOffers.events({
+  'submit form'(event) {
+    event.preventDefault();
+
+    const target = event.target;
+    const price = Number(target.price.value);
+    Meteor.call('makeOffer', "7rewJg6Ro9SS36eaL", Meteor.userId(), price);
+  }
+});
