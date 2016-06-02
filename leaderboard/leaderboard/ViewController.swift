d3e4a66c6a157a94e390c7c0e84eed7b44033bc0
Saurav  Mitra <saurav.mitra14@imperial.ac.uk>
2016-06-02 23:03:55 +0100

    Initalize leaderboard iOS test app

---
+++ leaderboard/leaderboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  leaderboard
+//
+//  Created by Saurav Mitra on 02/06/2016.
+//  Copyright © 2016 APTSD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
