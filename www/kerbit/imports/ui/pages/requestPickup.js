aa8ac2606e14b42cfbbb9227307f12460a6470e8
mak514 <mak514@ic.ac.uk>
2016-06-02 19:10:00 +0100

    show image thumbnail after selection

--- www/kerbit/imports/ui/pages/requestPickup.js
+++ www/kerbit/imports/ui/pages/requestPickup.js
@@ -4,7 +4,26 @@ import { Template } from 'meteor/templating';
 import '../../api/methods.js';
 import "./requestPickup.html";
 
+
+
+function readURL(input) {
+  console.log(input);
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $('#image').attr('src', e.target.result);
+    }
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
 Template.RequestPickup.events({
+  'change #file' (event) {
+    readURL(event.target);
+  },
+  
   'submit form'(event) {
     event.preventDefault();
 
