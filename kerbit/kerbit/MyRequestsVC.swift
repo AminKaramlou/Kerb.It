0cf8794e84ed35bec29f3cff79c428f976e2b72a
Saurav  Mitra <saurav.mitra14@imperial.ac.uk>
2016-06-03 15:29:47 +0100

    Modify storyboard

--- kerbit/kerbit/MyRequestsVC.swift
+++ kerbit/kerbit/MyRequestsVC.swift
@@ -1,15 +1,16 @@
 //
-//  MyRequestsVC.swift
-//  kerbit
+//  MyRequestsVCTableViewController.swift
+//
 //
 //  Created by Saurav Mitra on 03/06/2016.
-//  Copyright © 2016 APTSD. All rights reserved.
+//
 //
 
 import UIKit
 
 class MyRequestsVC: UIViewController, NSFetchedResultsControllerDelegate {
   var fRC: NSFetchedResultsController!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -26,32 +27,32 @@ class MyRequestsVC: UIViewController, NSFetchedResultsControllerDelegate {
   }
   
   @objc internal func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
-//      switch(type) {
-//      case .Insert:
-//        //changes!.append(.SectionInserted(sectionIndex))
-//      case .Delete:
-//        print(fRC.fetchedObjects)
-//        //changes!.append(.SectionDeleted(sectionIndex))
-//      default:
-//        break
-//      }
+    //      switch(type) {
+    //      case .Insert:
+    //        //changes!.append(.SectionInserted(sectionIndex))
+    //      case .Delete:
+    //        print(fRC.fetchedObjects)
+    //        //changes!.append(.SectionDeleted(sectionIndex))
+    //      default:
+    //        break
+    //      }
   }
   
   @objc internal func controller(controller: NSFetchedResultsController, didChangeObject object: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
-        switch(type) {
-        case .Insert:
-          print(fRC.fetchedObjects)
-          //changes!.append(.ObjectInserted(newIndexPath!))
-        case .Delete:
-          print(fRC.fetchedObjects)
-          //changes!.append(.ObjectDeleted(indexPath!))
-        case .Update:
-          print(fRC.fetchedObjects)
-          //changes!.append(.ObjectUpdated(indexPath!))
-        case .Move:
-          print(fRC.fetchedObjects)
-          //changes!.append(.ObjectMoved(indexPath: indexPath!, newIndexPath: newIndexPath!))
-        }
+    switch(type) {
+    case .Insert:
+      print(fRC.fetchedObjects)
+    //changes!.append(.ObjectInserted(newIndexPath!))
+    case .Delete:
+      print(fRC.fetchedObjects)
+    //changes!.append(.ObjectDeleted(indexPath!))
+    case .Update:
+      print(fRC.fetchedObjects)
+    //changes!.append(.ObjectUpdated(indexPath!))
+    case .Move:
+      print(fRC.fetchedObjects)
+      //changes!.append(.ObjectMoved(indexPath: indexPath!, newIndexPath: newIndexPath!))
+    }
   }
   
   @objc internal func controllerDidChangeContent(controller: NSFetchedResultsController) {
