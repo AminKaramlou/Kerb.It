5c6a32f42bca6aa16411bc30f40a6bd72122909e
mak514 <mak514@ic.ac.uk>
2016-06-03 04:13:22 +0100

    Fix problem with server tests

--- www/kerbit/imports/startup/client/google_map.js
+++ www/kerbit/imports/startup/client/google_map.js
@@ -48,7 +48,7 @@ Template.map.helpers({
     if (GoogleMaps.loaded()) {
       return {
         center: new google.maps.LatLng(51.5074, -0.1278),
-        zoom: 9
+        zoom: 12
       };
     }
   }
