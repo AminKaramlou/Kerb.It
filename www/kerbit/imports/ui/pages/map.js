ce3fd7f0b183a7ec56ee5fae9ff13f03779c2078
mak514 <mak514@ic.ac.uk>
2016-06-03 11:54:58 +0100

    map defaults to current location

--- www/kerbit/imports/ui/pages/map.js
+++ www/kerbit/imports/ui/pages/map.js
@@ -12,14 +12,13 @@ Template.map.onCreated(function (){
         handleNoGeolocation(browserSupportFlag);
       });
     }
-  })gu;
+  });
 });
 
 
 Template.map.helpers({
   mapOptions: function() {
     if (GoogleMaps.loaded()) {
-
 
       return {
         center: new google.maps.LatLng(51.5074, -0.1278),
