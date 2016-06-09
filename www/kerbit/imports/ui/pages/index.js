74d1bd15a67942dfcf3494f7e4bc31d497569396
Paul Benn <pb2513@ic.ac.uk>
2016-06-09 18:19:30 +0100

    [PB] Work on accountSettings styles

--- www/kerbit/imports/ui/pages/index.js
+++ www/kerbit/imports/ui/pages/index.js
@@ -11,6 +11,7 @@ import "./driverHistory.js";
 import "./clientHistory.js";
 import "./userProfilePublicView.js"
 import "./customSettings.js"
+//import "./experiment.js"
 
 import "../components/onlyDriver.js";
 import "../components/onlyConsumer.js";
