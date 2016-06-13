1f961419f88988c25e0a3804e0157d6cbaadd19d
Tom Szyszko <ts3413@ic.ac.uk>
2016-06-13 21:19:38 +0100

    started map display

--- www/kerbit/imports/ui/pages/makeOffers.js
+++ www/kerbit/imports/ui/pages/makeOffers.js
@@ -7,48 +7,79 @@ import { Requests } from '../../api/collections/requests.js';
 import '../../api/methods.js';
 import "./makeOffers.html";
 
+var src = 'https://www.kerbit.co.uk/london.kml';
+
+function loadKmlLayer(src, map) {
+  var kmlLayer = new google.maps.KmlLayer(src, {
+    suppressInfoWindows: true,
+    preserveViewport: false,
+    map: map
+  });
+  kmlLayer.setMap(map);
+  google.maps.event.addListener(kmlLayer, 'click', function(event) {
+    var content = event.featureData.name;
+    kmlLayer.setOptions({fillOpacity:1});
+    alert(content);
+
+  });
+  google.maps.event.addListener(kmlLayer, 'mouseover', function(event) {
+    this.setOptions({fillColor: "#00FF00"});
+    tooltip.style.visibility = 'visible';
+});
+
+google.maps.event.addListener(kmlLayer, 'mouseout', function() {
+    kmlLayer.setOptions({fillOpacity:0.0});
+});
+}
+
 Template.MakeOffersHelper.onCreated(function driverHomeOnCreated() {
   Meteor.subscribe('requests');
   Meteor.subscribe('images');
   Meteor.subscribe('items');
-
   GoogleMaps.ready('map', function(map) {
-    var directionsServices = {};
-    var directionsDisplays = {};
-
-    Requests.find({isActive: true}).observe({
-      added: function (document) {
-
-        directionsServices[document._id] = new google.maps.DirectionsService;
-        directionsDisplays[document._id] = new google.maps.DirectionsRenderer;
-        directionsDisplays[document._id].setMap(map.instance);
-
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function (position) {
-            currentPos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-            directionsServices[document._id].route({
-              origin: currentPos,
-              destination: new google.maps.LatLng(document.loc.coordinates[1], document.loc.coordinates[0]),
-              travelMode: google.maps.TravelMode.DRIVING,
-            }, function (response, status) {
-              if (status === google.maps.DirectionsStatus.OK) {
-                directionsDisplays[document._id].setDirections(response);
-              } else {
-                window.alert('Directions request failed due to ' + status);
-              }
-            });
-          })
-        }
-      },
-
-      removed: function (oldDocument) {
-        directionsDisplays[oldDocument._id].setMap(null);
-        delete directionsDisplays[oldDocument._id];
-        delete directionsServices[oldDocument._id];
-      }
+    console.log("gets here");
+      loadKmlLayer(src, map.instance);
+    console.log("function runs");
     });
+
   });
-});
+
+  // GoogleMaps.ready('map', function(map) {
+  //   var directionsServices = {};
+  //   var directionsDisplays = {};
+  //
+  //   Requests.find({isActive: true}).observe({
+  //     added: function (document) {
+  //
+  //       directionsServices[document._id] = new google.maps.DirectionsService;
+  //       directionsDisplays[document._id] = new google.maps.DirectionsRenderer;
+  //       directionsDisplays[document._id].setMap(map.instance);
+  //
+  //       if (navigator.geolocation) {
+  //         navigator.geolocation.getCurrentPosition(function (position) {
+  //           currentPos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  //           directionsServices[document._id].route({
+  //             origin: currentPos,
+  //             destination: new google.maps.LatLng(document.loc.coordinates[1], document.loc.coordinates[0]),
+  //             travelMode: google.maps.TravelMode.DRIVING,
+  //           }, function (response, status) {
+  //             if (status === google.maps.DirectionsStatus.OK) {
+  //               directionsDisplays[document._id].setDirections(response);
+  //             } else {
+  //               window.alert('Directions request failed due to ' + status);
+  //             }
+  //           });
+  //         })
+  //       }
+  //     },
+  //
+  //     removed: function (oldDocument) {
+  //       directionsDisplays[oldDocument._id].setMap(null);
+  //       delete directionsDisplays[oldDocument._id];
+  //       delete directionsServices[oldDocument._id];
+  //     }
+  //   });
+  // });
 
 Template.MakeOffersHelper.helpers({
 
@@ -75,6 +106,17 @@ Template.MakeOffersHelper.helpers({
           }
         }
       });
+    }
+  },
+
+  mapOptions: function() {
+    if (GoogleMaps.loaded()) {
+
+      return {
+        center: new google.maps.LatLng(51.5074, -0.1278),
+        zoom: 15,
+        minZoom: 2
+      };
     }
   }
 });
