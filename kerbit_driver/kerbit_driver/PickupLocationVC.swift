261d859bd770d10008ccd5baaafbd29347049596
Saurav  Mitra <saurav.mitra14@imperial.ac.uk>
2016-06-15 04:43:33 +0100

    Add routes to Google Maps in driver app

--- kerbit_driver/kerbit_driver/PickupLocationVC.swift
+++ kerbit_driver/kerbit_driver/PickupLocationVC.swift
@@ -13,6 +13,49 @@ class PickupLocationVC: UIViewController {
   let locationManager = CLLocationManager()
   var next: FormVC!
   
+  func drawRoute(origin: CLLocation, destination: CLLocation) {
+    fetchPolylineWithOrigin(origin, destination: destination) { polyline in
+      let originMarker = GMSMarker(position: origin.coordinate)
+      originMarker.map = self.mapView
+      let destinationMarker = GMSMarker(position: destination.coordinate)
+      destinationMarker.map = self.mapView
+      polyline.map = self.mapView
+    }
+  }
+  
+  func fetchPolylineWithOrigin(origin: CLLocation, destination: CLLocation,
+                               completionHandler: GMSPolyline->Void) {
+    let originString = "\(origin.coordinate.latitude),\(origin.coordinate.longitude)"
+    let destinationString = "\(destination.coordinate.latitude),\(destination.coordinate.longitude)"
+    let directionsAPI = "https://maps.googleapis.com/maps/api/directions/json?"
+    let directionsUrlString = "\(directionsAPI)&origin=\(originString)&destination=\(destinationString)&mode=driving"
+    let directionsUrl = NSURL(string: directionsUrlString)!
+  
+    print (directionsUrl)
+    let fetchDirectionsTask = NSURLSession.sharedSession().dataTaskWithURL(directionsUrl) {
+      (data, response, error) in
+      let json = try! NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+      if (error != nil) {
+        return;
+      }
+      
+      let routesArray = json.objectForKey("routes")!
+  
+      if (routesArray.count > 0) {
+        let routeDict = routesArray[0]
+        let routeOverviewPolyline = routeDict.objectForKey("overview_polyline")!
+        let points = routeOverviewPolyline.objectForKey("points") as! String
+        dispatch_async(dispatch_get_main_queue()) {
+          let path = GMSPath(fromEncodedPath: points)
+          let polyline = GMSPolyline(path: path)
+          completionHandler(polyline)
+        }
+      }
+    }
+    
+    fetchDirectionsTask.resume()
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     locationManager.delegate = self
@@ -22,9 +65,12 @@ class PickupLocationVC: UIViewController {
   }
   
   @IBAction func didSetLocation(sender: AnyObject) {
-    next.latitude = mapView.camera.target.latitude
-    next.longitude = mapView.camera.target.longitude
-    presentViewController(next, animated: true, completion: nil)
+    let origin = CLLocation(latitude: mapView.camera.target.latitude, longitude: mapView.camera.target.longitude)
+    let destination = CLLocation(latitude: mapView.camera.target.latitude + 2, longitude: mapView.camera.target.longitude)
+    drawRoute(origin, destination: destination)
+//    next.latitude = mapView.camera.target.latitude
+//    next.longitude = mapView.camera.target.longitude
+//    presentViewController(next, animated: true, completion: nil)
   }
 }
 
@@ -33,7 +79,7 @@ extension PickupLocationVC: CLLocationManagerDelegate {
     if status == .AuthorizedWhenInUse {
     }
   }
-    
+  
   func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
     if let location = locations.first {
       mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
